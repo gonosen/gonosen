8e6211cf96dddfff8100d12653c2aa9de5b1916f
tennmoku71 <tennmoku71@gmail.com>
2016-12-04 23:13:44 +0900

    intern index view design

--- db/schema.rb
+++ db/schema.rb
@@ -34,13 +34,8 @@ ActiveRecord::Schema.define(version: 20161123081031) do
   create_table "interns", force: :cascade do |t|
     t.string   "title"
     t.text     "context"
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.string   "location"
-    t.string   "owner"
-    t.integer  "intern_type"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
