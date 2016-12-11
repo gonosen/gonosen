4e6857037c6df762dcd41729f0c7d67e91071096
Yoshiki Ohira <tennmoku71@gmail.com>
2016-12-12 04:56:47 +0900

    webric succees

---
+++ vendor/bundle/ruby/2.2.0/gems/simple_calendar-2.2.1/lib/simple_calendar/version.rb
@@ -0,0 +1,3 @@
+module SimpleCalendar
+  VERSION = "2.2.1"
+end
---
+++ vendor/bundle/ruby/2.2.0/gems/simple_calendar-2.2.1/lib/simple_calendar/week_calendar.rb
@@ -0,0 +1,21 @@
+module SimpleCalendar
+  class WeekCalendar < SimpleCalendar::Calendar
+    def week_number
+      format = (Date.beginning_of_week == :sunday) ? "%U" : "%W"
+      start_date.strftime(format).to_i
+    end
+
+    private
+
+      def date_range
+        starting = start_date.beginning_of_week
+        ending = (starting + (number_of_weeks - 1).weeks).end_of_week
+
+        (starting..ending).to_a
+      end
+
+      def number_of_weeks
+        options.fetch(:number_of_weeks, 1)
+      end
+  end
+end
